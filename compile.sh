4d5c9cef5732e05bca2f0d0a5a162440f07b26df
Peter Blum <Oskoss@users.noreply.github.com>
2024-04-22 08:49:14 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -46,3 +46,24 @@ cp ../EQ2/source/WorldServer/eq2world ./ && cp ../EQ2/source/LoginServer/login .
 cp -rT ../server .
 
 ls -ltra
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!!!!!!!Yay, it compiled and built!!!!!!!!!!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+if [[ -z "${GITHUB_ACCESS_TOKEN}" ]]; then
+  echo "GITHUB_ACCESS_TOKEN not set....update and try again"
+fi
+
+REPO_REMOTE=$(git config --get remote.origin.url)
+REPO_NAME=$(basename -s .git $REPO_REMOTE)
+VERSION=$(git log --pretty=format:'%H' -n 1)
+MESSAGE=$(printf "Release of version %s" $VERSION)
+REPO_OWNER="oskoss"
+BRANCH="main"
+DRAFT="false"
+PRE="false"
+
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "%s","name": "%s","body": "%s","draft": %s,"prerelease": %s}' "$VERSION" "$BRANCH" "$VERSION" "$MESSAGE" "$DRAFT" "$PRE" )
+API_RESPONSE_STATUS=$(curl --data "$API_JSON" -s -i https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases?access_token=$GITHUB_ACCESS_TOKEN)
+echo "$API_RESPONSE_STATUS"
