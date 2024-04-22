9a47de36929aa7c18a4bf0533e55ee0e079b0ce4
Peter Blum <4152407+oskoss@users.noreply.github.com>
2024-04-22 10:31:19 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,40 +5,40 @@ echo "Today is " `date -u --date=@1404372514`
 
 cd ~
 
-# git clone --depth 1 https://git.eq2emu.com/devn00b/EQ2EMu.git
+git clone --depth 1 https://git.eq2emu.com/devn00b/EQ2EMu.git
 
-# cd ~/EQ2EMu/EQ2/source/depends/recastnavigation/RecastDemo
+cd ~/EQ2EMu/EQ2/source/depends/recastnavigation/RecastDemo
 
-# curl -OL https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-linux.tar.gz
+curl -OL https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-linux.tar.gz
 
-# tar -xvf premake-5.0.0-beta2-linux.tar.gz
+tar -xvf premake-5.0.0-beta2-linux.tar.gz
 
-# #Fix some issue with altStackMem size
-# sed -i -e 's/SIGSTKSZ/32768/g' ~/EQ2EMu/EQ2/source/depends/recastnavigation/Tests/catch.hpp
+#Fix some issue with altStackMem size
+sed -i -e 's/SIGSTKSZ/32768/g' ~/EQ2EMu/EQ2/source/depends/recastnavigation/Tests/catch.hpp
 
-# ./premake5 gmake
+./premake5 gmake
 
-# cd Build/gmake
+cd Build/gmake
 
-# make
+make
 
-# cd ../../../..
+cd ../../../..
 
-# git clone https://github.com/fmtlib/fmt.git
+git clone https://github.com/fmtlib/fmt.git
 
-# cd ~/EQ2EMu/EQ2/source/LoginServer/
+cd ~/EQ2EMu/EQ2/source/LoginServer/
 
-# make -j$(nproc)
+make -j$(nproc)
 
-# cd ~/EQ2EMu/EQ2/source/WorldServer
+cd ~/EQ2EMu/EQ2/source/WorldServer
 
-# make -j$(nproc)
+make -j$(nproc)
 
-# cd /workspace
+cd /workspace
 
-# cp ~/EQ2EMu/EQ2/source/WorldServer/eq2world ./ && cp ~/EQ2EMu/EQ2/source/LoginServer/login ./
+cp ~/EQ2EMu/EQ2/source/WorldServer/eq2world ./ && cp ~/EQ2EMu/EQ2/source/LoginServer/login ./
 
-# cp -rT ~/EQ2EMu/server .
+cp -rT ~/EQ2EMu/server .
 
 ls -ltra
 
