bb26c8e1fc0910d55a8f1931d50d9d968f042c09
Peter Blum <4152407+oskoss@users.noreply.github.com>
2024-04-22 13:09:36 -0500

    Update release.sh

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ RELEASE_JSON=$(printf '{"tag_name": "%s","target_commitish": "%s","name": "%s","
 RELEASE_RESPONSE_STATUS=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases -d "$RELEASE_JSON")
 echo "$RELEASE_RESPONSE_STATUS"
 
-UPLOAD_URL=$(printf $RELEASE_RESPONSE_STATUS | awk -F "[ ]+" '/upload_url/{print $NF}')
+UPLOAD_URL=$(echo $RELEASE_RESPONSE_STATUS | awk -F "[ ]+" '/upload_url/{print $NF}')
 echo "$UPLOAD_URL"
 UPLOAD_URL="${UPLOAD_URL:1:-15}"
 
