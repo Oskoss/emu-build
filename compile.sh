158ba5729ca712e488a83e0a8f240f143d251bad
Peter Blum <4152407+oskoss@users.noreply.github.com>
2024-05-02 09:56:03 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ curl -OL https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/
 tar -xvf premake-5.0.0-beta2-linux.tar.gz
 
 #Fix some issue with altStackMem size
-sed -i -e 's/SIGSTKSZ/32768/g' ~/EQ2EMu/EQ2/source/depends/recastnavigation/Tests/catch.hpp
+sed -i -e 's/SIGSTKSZ/32768/g' /workspace/EQ2EMu/EQ2/source/depends/recastnavigation/Tests/catch.hpp
 
 ./premake5 gmake
 
